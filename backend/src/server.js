22d93b6fa6a5656ec49411501e32e3340ad342ff
Prakash <rprakashproz@gmail.com>
2026-07-22 15:04:03 +0530

    Simplify server startup by removing database connection
    Removed database connection from server bootstrap process.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,7 @@
 import app from "./app.js";
 import env from "./config/env.js";
 import logger from "./utils/logger.js";
-import connectDatabase from "./config/database.js";
 
-const bootstrap = async () => {
-    await connectDatabase();
-
-    app.listen(env.PORT, () => {
-        logger.info(`Server Started on Port ${env.PORT}`);
-    });
-};
-
-bootstrap();
+app.listen(env.PORT, () => {
+    logger.info(`Server Started on Port ${env.PORT}`);
+});
