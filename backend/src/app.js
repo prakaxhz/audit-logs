b562ced1b6efed13b68d7158044da8c285c932f7
Prakash <rprakashproz@gmail.com>
2026-07-22 15:03:17 +0530

    Implement database connection middleware
    Add database connection middleware to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import logger from "./utils/logger.js";
 import routes from "./routes/index.js";
 import notFoundMiddleware from "./middlewares/notFound.middleware.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import connectDatabase from "./config/database.js";
 
 const app = express();
 
@@ -30,6 +31,15 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
+app.use(async (req, res, next) => {
+    try {
+        await connectDatabase();
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 app.use("/api", routes);
 
